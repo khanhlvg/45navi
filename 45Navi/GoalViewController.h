24da149afcb76791d0fbfdc20410d869642515df
yoshik <fujiwara@yoshiki.mobi>
2014-11-16 08:24:09 +0900

    impl GoalView

---
+++ 45Navi/GoalViewController.h
@@ -0,0 +1,13 @@
+//
+//  GoalViewController.h
+//  45Navi
+//
+//  Created by yoshiki on 2014/11/16.
+//  Copyright (c) 2014年 45Navi Team. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GoalViewController : UIViewController
+
+@end
