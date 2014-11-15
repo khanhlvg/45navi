eaae46bb43149093c623b6f7eaa565d3ae5aa2b4
yoshik <fujiwara@yoshiki.mobi>
2014-11-15 18:46:14 +0900

    article view likes tinder

---
+++ 45Navi/ArticleViewController.h
@@ -0,0 +1,13 @@
+//
+//  ArticleViewController.h
+//  45Navi
+//
+//  Created by yoshiki on 2014/11/15.
+//  Copyright (c) 2014年 45Navi Team. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ArticleViewController : UIViewController
+
+@end
