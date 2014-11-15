419e873c2f500845ebd7e92f5caaf1c35645502b
Tran Thanh Thuy <thuytran@Trans-MacBook-Air.local>
2014-11-15 16:08:30 +0900

    Initial Commit

---
+++ 45Navi/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  45Navi
+//
+//  Created by Tran Thanh Thuy on 11/15/14.
+//  Copyright (c) 2014 45Navi Team. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ 45Navi/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  45Navi
+//
+//  Created by Tran Thanh Thuy on 11/15/14.
+//  Copyright (c) 2014 45Navi Team. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
